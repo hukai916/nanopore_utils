ae853349dfa94ac654e4d084d063e42f37c4a38c
Kai Hu <>
2022-02-11 14:38:06 -0500

    minor

--- scripts/get_eventalign.py
+++ scripts/get_eventalign.py
@@ -10,6 +10,7 @@ python get_eventalign.py fast5_dir match_fasta_ref.txt outdir hpc/local
 import sys
 import os
 import subprocess
+import pathlib
 
 fast5_dir = sys.argv[1]
 match_fasta_ref_file = sys.argv[2]
@@ -29,7 +30,13 @@ samtools_index   = "samtools index " + outdir + "/" + _5mer + ".sorted.bam"
 nanopolish_align = "nanopolish eventalign --reads " + fasta + " --bam " + outdir + "/" + _5mer + ".sorted.bam --genome " + ref + " --scale-events > " + outdir + "/" +_5mer + ".eventalign.txt"
 
 commands = ["module load minimap2/2.17", "module load samtools/1.4.1", "module load nanopolish/v0.13.2", nanopolish_index, minimap2, samtools_index, nanopolish_align]
+pathlib.Path(outdir).mkdir(parents=True, exist_ok=True)
+command  = " && ".join(commands)
 
 if executor == "local":
-    command = " && ".join(commands)
-    print(command)
+    print(_5mer, " running ...")
+    subprocess.call(command, shell = True)
+    print(_5mer, " done")
+else if executor == "hpc":
+    command = 'bsub -q short -n 1 -W 4:00 -R select[rh=6] -R rusage[mem=4000] -o ' + outdir + "/" + _5mer + ".log " + '"' + command  + '"'
+    print(command) 
