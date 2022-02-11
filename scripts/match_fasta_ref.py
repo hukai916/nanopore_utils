2be733fbe4709aae84c4ca04cd86f67facfd167f
Kai Hu <>
2022-02-11 11:30:53 -0500

    minor

--- scripts/match_fasta_ref.py
+++ scripts/match_fasta_ref.py
@@ -18,9 +18,10 @@ pathlib.Path(outdir).mkdir(parents=True, exist_ok=True)
 match_fasta_ref = {}
 fasta_files = glob.glob(fasta_dir + "/reads_fasta_*.fasta")
 ref_files   = glob.glob(ref_dir + "/ref_*.fasta")
+print(fasta_files)
+print(fastq_dir)
 
 for fasta in fasta_files:
-    print(fasta)
     _5mer = fasta.split("_")[-1].split(".fasta")[0]
     if not _5mer in match_fasta_ref:
         match_fasta_ref[_5mer] = [fasta]
